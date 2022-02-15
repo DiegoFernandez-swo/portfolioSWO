8cd22b53d74a477d490ed98f05f18a24416301a9
dieferorSWO <diego.fernandez@softwareone.com>
2022-02-15 10:29:20 +0100

    version 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,43 @@ const menu = document.querySelector('.nav .menu');
 const nav = document.querySelector('.nav');
 const navLink = document.querySelectorAll('.nav-link');
 
-//burger menu-toggle
-burger.addEventListener("click",() => {
-    menu.classList.toggle("active");    
-});
+// //burger menu-toggle
+// burger.addEventListener("click",() => {
+//     menu.classList.toggle("active");    
+// });
 
 //close menu when link is clicked
 navLink.forEach((link) => 
     link.addEventListener("click", () =>{
-        menu.classList.remove("active");
+        menu.classList.remove("active");         
     })
 );
+$(document).ready(function(){
+    //toggle burger menu
+    $(burger).click(function(){
+        $(menu).toggleClass("active");
+        $('.burger-menu i').toggleClass("active");
+    });
+    //owl carousel
+    $('.carousel').owlCarousel({
+        margin: 20,
+        nav: true,       
+        // loop: true,
+        // autoplayTimeOut: 2000,
+        // autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
